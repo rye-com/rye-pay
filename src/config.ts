6eaa775f70e722575046dc4a104a8a7277cf287e
Sophia Willows <20146550+sophiabits@users.noreply.github.com>
2024-04-13 15:24:22 +1200

    Fix production audience value

--- src/config.ts
+++ src/config.ts
@@ -8,15 +8,15 @@ interface EnvironmentConfig {
 export const config = {
   environments: {
     local: {
-      url: 'http://localhost:3000/v1/query',
       audience: 'dev.api.rye.com',
+      url: 'http://localhost:3000/v1/query',
     },
     staging: {
       audience: 'staging.graphql.api.rye.com',
       url: process.env.CART_API_STAGING_URL ?? 'https://staging.graphql.api.rye.com/v1/query',
     },
     production: {
-      audience: 'dev.api.rye.com',
+      audience: 'graphql.api.rye.com',
       url: process.env.CART_API_PRODUCTION_URL ?? 'https://graphql.api.rye.com/v1/query',
     },
   } satisfies Record<Environment, EnvironmentConfig>,
